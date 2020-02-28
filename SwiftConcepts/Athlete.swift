6e6b36002258d69613ab82aa0d257609f8f8cd0d
Usuário Convidado <Guest@PA06MICRO03.local>
2020-02-28 09:41:35 -0300

    first commit

---
+++ SwiftConcepts/Athlete.swift
@@ -0,0 +1,13 @@
+//
+//  Athlete.swift
+//  SwiftConcepts
+//
+//  Created by Usuário Convidado on 28/02/20.
+//  Copyright © 2020 Wilziin. All rights reserved.
+//
+
+import Cocoa
+
+class Athlete: NSObject {
+
+}
